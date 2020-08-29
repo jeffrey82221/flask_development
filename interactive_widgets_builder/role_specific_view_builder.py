f7806c965e5c2a09b49834011b69a483996c5b5d
jeffrey82221 <jeffrey82221@gmail.com>
2020-08-29 18:58:15 +0800

    Refactorize role_specific_view_builder into ViewFactory class

--- interactive_widgets_builder/role_specific_view_builder.py
+++ interactive_widgets_builder/role_specific_view_builder.py
@@ -7,7 +7,67 @@ from interactive_widgets_builder.table_randerer_interface import TableOrganizer
 from interactive_widgets_builder.table_view_builder import TableViewBuilder
 
 
-def building_admin_view(platform='jupyter'):
+class ViewFactory():
+  def __init__(self):
+    self.view_types = ['all', 'project', 'member']
+    self.randerer_flows = {
+        'all-1': [
+            TableRanderer.get_project_level_table_info,
+            TableRanderer.get_project_specific_member_level_table_info,
+            TableRanderer.get_project_member_specific_table_info
+        ],
+        'all-2': [
+            TableRanderer.get_member_level_table_info,
+            TableRanderer.get_member_specific_table_info
+        ],
+        'all-3': [TableRanderer.get_table_info],
+        'project-1': [
+            TableRanderer.get_project_specific_member_level_table_info,
+            TableRanderer.get_project_member_specific_table_info
+        ],
+        'project-2': [TableRanderer.get_project_specific_table_info],
+        'member': [TableRanderer.get_member_specific_table_info]
+    }
+    self.level_names_config = {
+        'all-1': ['project', 'member', 'table'],
+        'all-2': ['member', 'table'],
+        'all-3': ['table'],
+        'project-1': ['member', 'table'],
+        'project-2': ['table'],
+        'member': ['table']
+    }
+    self.mode_of_each_view_type = {
+        'all': ['all-1', 'all-2', 'all-3'],
+        'project': ['project-1', 'project-2'],
+        'member': ['member']
+    }
+
+  def build_view(self, view_type, condition, platform='jupyter'):
+    randerer_flow_keys = self.mode_of_each_view_type[view_type]
+    table_widget_list = []
+    for i, key in enumerate(randerer_flow_keys):
+      tvb = TableViewBuilder(
+          TableOrganizer(table_randerers=self.randerer_flows[key],
+                         level_names=self.level_names_config[key]))
+      table_widget = tvb.build_table_widget()
+      table_widget_list.append(table_widget)
+    tab_nest = widgets.Tab()
+    tab_nest.children = table_widget_list
+    for i, key in enumerate(randerer_flow_keys):
+      tab_nest.set_title(
+          i, '{level_name}-level information'.format(
+              level_name=self.level_names_config[key][0]))
+    if platform == 'flask':
+      temp_file_name = '{view_type}.html'.format(view_type=view_type)
+      embed_minimal_html(temp_file_name,
+                         views=[tab_nest],
+                         title='DB Information')
+      return open(temp_file_name).read()
+    else:  # platform == jupyter
+      return tab_nest
+
+
+'''def building_admin_view(platform='jupyter'):
   assert platform == 'jupyter' or platform == 'flask'
   tvb = TableViewBuilder(TableOrganizer(
       table_randerers=[
@@ -53,10 +113,6 @@ def building_admin_view(platform='jupyter'):
 
 def building_pm_view(project_name, platform='jupyter'):
   assert platform == 'jupyter' or platform == 'flask'
-  '''project_level_table_view = build_table_widget(
-      current_level = 'project',
-      with_subtable = True,
-      next_level = 'member')'''
   tvb = TableViewBuilder(TableOrganizer(
       table_randerers=[
           TableRanderer.get_project_specific_member_level_table_info,
@@ -107,4 +163,4 @@ def building_member_view(member_name, platform='jupyter'):
     embed_minimal_html(member_name + '.html', views=[tab_nest], title=member_name)
     return open(member_name + '.html').read()
   else:  # platform == jupyter
-    return VBox([Label(value="Member: " + member_name), tab_nest])
+    return VBox([Label(value="Member: " + member_name), tab_nest])'''
