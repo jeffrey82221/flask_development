b7ab4eca86d09016af310fa737dc9ea67b64ea68
jeffrey82221 <jeffrey82221@gmail.com>
2020-08-29 16:38:42 +0800

    Adapting the new TableViewBuilder in role_specific_view_builder.

--- interactive_widgets_builder/role_specific_view_builder.py
+++ interactive_widgets_builder/role_specific_view_builder.py
@@ -1,27 +1,45 @@
 from ipywidgets import widgets, VBox, Label
 from ipywidgets.embed import embed_minimal_html
 
+from table_info_extractor.table_rander import TableRanderer
+
+from interactive_widgets_builder.table_randerer_interface import TableOrganizer
 from interactive_widgets_builder.table_view_builder import TableViewBuilder
 
 
 def building_admin_view(platform='jupyter'):
-  tvb = TableViewBuilder()
   assert platform == 'jupyter' or platform == 'flask'
-  project_level_table_view = tvb.build_top_table_widget(
-      current_level='project',
-      with_subtable=True,
-      next_level='member')
-  member_level_table_view = tvb.build_top_table_widget(
-      current_level='member',
-      with_subtable=True,
-      next_level='table')
-  table_view = tvb.build_top_table_widget(
-      current_level='table')
+  tvb = TableViewBuilder(TableOrganizer(
+      table_randerers=[
+          TableRanderer.get_project_level_table_info,
+          TableRanderer.get_project_specific_member_level_table_info,
+          TableRanderer.get_project_member_specific_table_info
+      ],
+      level_names=['project', 'member', 'table']
+  ))
+  project_level_table_view = tvb.build_table_widget()
+
+  tvb = TableViewBuilder(TableOrganizer(
+      table_randerers=[
+          TableRanderer.get_member_level_table_info,
+          TableRanderer.get_member_specific_table_info
+      ],
+      level_names=['member', 'table']
+  ))
+  member_level_table_view = tvb.build_table_widget()
+  tvb = TableViewBuilder(TableOrganizer(
+      table_randerers=[
+          TableRanderer.get_table_info
+      ],
+      level_names=['table']
+  ))
+  table_level_view = tvb.build_table_widget()
+
   tab_nest = widgets.Tab()
   tab_nest.children = [
       project_level_table_view,
       member_level_table_view,
-      table_view
+      table_level_view
   ]
   tab_nest.set_title(0, 'project-level information')
   tab_nest.set_title(1, 'member-level information')
