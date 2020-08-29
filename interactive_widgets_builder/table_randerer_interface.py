560f8a65b7c8009343112c45e501d41d2c2898a9
jeffrey82221 <jeffrey82221@gmail.com>
2020-08-29 11:34:48 +0800

    Add new essential argument to table_randerer_interface

--- interactive_widgets_builder/table_randerer_interface.py
+++ interactive_widgets_builder/table_randerer_interface.py
@@ -1,3 +1,11 @@
 class Organizer():
-  def __init__(self, table_randerers):
+  def __init__(self, table_randerers, level_names):
+    '''
+    Argument:
+     - table_randerers: a list of methods in TableRanderer listed
+        from root level to leaf level.
+     - level_names: the name of the level from root to leaf,
+        e.g., ["group", "schema", "table"]
+    '''
     self.table_randerers = table_randerers
+    self.level_names = level_names
