eaf24a5afc1de3f88d430588d3b2c7adc10125c8
jeffrey82221 <jeffrey82221@gmail.com>
2020-08-30 11:03:20 +0800

    Draw configuration __init__ for ViewController

--- interactive_widgets_builder/ipywidget_view_controller.py
+++ interactive_widgets_builder/ipywidget_view_controller.py
@@ -12,7 +12,7 @@ class Request():
     self.values = values
 
 
-class InteractiveDropdown():
+class _InteractiveDropdown():
   def _pass_info(self, selected_result):
     self._selected_result = selected_result
 
@@ -46,7 +46,7 @@ class SelectionView():
 
   def __init__(self, title, options):
     self._title = title
-    self._interactive_dropdown = InteractiveDropdown(title, options)
+    self._interactive_dropdown = _InteractiveDropdown(title, options)
     self._button = widgets.Button(
         value=False,
         description='Enter',
@@ -61,66 +61,103 @@ class SelectionView():
     return HBox([self._interactive_dropdown.get_widget(), self._button])
 
 
-view_factory = ViewFactory()
-
-
 class ViewController():
-  @staticmethod
-  def entry():
-    sv = SelectionView('Role', ['Admin', 'PM', 'Member'])
-    return sv.get_widget()
+  def __init__():
+    self.dropdown_titles = ['Role', 'PM', 'Member']
+    self._options_for_each_dropdown_titles = {
+        'Role': ['Admin', 'PM', 'Member'],
+        'PM': ToyDB.project_list,
+        'Member': ToyDB.unique_members
+    }
+    self.view_factory = ViewFactory()
+    self.entry_dropdown_title = 'Role'
 
-  @staticmethod
-  def response_by_altering_view(request):
+  def entry(self):
+    sv = SelectionView(
+        self.entry_dropdown_title,
+        self._options_for_each_dropdown_titles[self.entry_dropdown_title]
+    )
+    widget = sv.get_widget()
+    return widget
+
+  def _get_reordered_option_list(self, dropdown_title, selected_result):
+    import copy
+    options = copy.copy(self._options_for_each_dropdown_titles[dropdown_title])
+    options.remove(selected_result)
+    return [selected_result] + options
+
+  def response_by_altering_view(self, request):
     import copy
     clear_output(wait=True)
     if 'Role' in request.values.keys():
       if request.values['Role'] == 'PM':
-        sv1 = SelectionView('Role', ['PM', 'Admin', 'Member'])
-        sv2 = SelectionView('PM', ToyDB.project_list)
+        sv1 = SelectionView(
+            'Role',
+            self._get_reordered_option_list('Role', 'PM')
+        )
+        sv2 = SelectionView(
+            'PM',
+            self._options_for_each_dropdown_titles['PM']
+        )
         widget = VBox([
             sv1.get_widget(),
             sv2.get_widget()])
       elif request.values['Role'] == 'Member':
-        sv1 = SelectionView('Role', ['Member', 'Admin', 'PM'])
-        sv2 = SelectionView('Member', ToyDB.unique_members)
+        sv1 = SelectionView(
+            'Role',
+            self._get_reordered_option_list('Role', 'Member')
+        )
+        sv2 = SelectionView(
+            'Member',
+            self._options_for_each_dropdown_titles['Member']
+        )
         widget = VBox([
             sv1.get_widget(),
             sv2.get_widget()
         ])
       else:  # Role: Admin
-        sv = SelectionView('Role', ['Admin', 'PM', 'Member'])
+        sv = SelectionView(
+            'Role',
+            self._options_for_each_dropdown_titles['Role']
+        )
         widget = VBox([
             sv.get_widget(),
-            view_factory.build_view('all')
+            self.view_factory.build_view('all')
         ])
         # print("Show Admin Table View")
 
     else:
       if 'PM' in request.values.keys():
-        project = request.values['PM']
-        sv1 = SelectionView('Role', ['PM', 'Admin', 'Member'])
-
-        list_here = copy.copy(ToyDB.project_list)
-        list_here.remove(project)
-        sv2 = SelectionView('PM', [project] + list_here)
+        selected_project = request.values['PM']
+        sv1 = SelectionView(
+            'Role',
+            self._get_reordered_option_list('Role', 'PM')
+        )
+        sv2 = SelectionView(
+            'PM',
+            self._get_reordered_option_list('PM', selected_project)
+        )
         widget = VBox([
             sv1.get_widget(),
             sv2.get_widget(),
-            view_factory.build_view('project', project)
+            self.view_factory.build_view('project', selected_project)
         ])
         # print("Show PM Table View for", project)
 
       elif 'Member' in request.values.keys():
-        member = request.values['Member']
-        sv1 = SelectionView('Role', ['Member', 'Admin', 'PM'])
-        list_here = copy.copy(ToyDB.unique_members)
-        list_here.remove(member)
-        sv2 = SelectionView('Member', [member] + list_here)
+        selected_member = request.values['Member']
+        sv1 = SelectionView(
+            'Role',
+            self._get_reordered_option_list('Role', 'Member')
+        )
+        sv2 = SelectionView(
+            'Member',
+            self._get_reordered_option_list('Member', selected_member)
+        )
         widget = VBox([
             sv1.get_widget(),
             sv2.get_widget(),
-            view_factory.build_view('member', member)
+            self.view_factory.build_view('member', selected_member)
         ])
         # print("Show Member Table View for", member)
     display(widget)
