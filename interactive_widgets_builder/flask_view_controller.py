29ec395d6443312d5b4113109199114964e813b6
jeffrey82221 <jeffrey82221@gmail.com>
2020-08-30 16:13:28 +0800

    Merge Ipywidget displaying function with Flask ViewController

--- interactive_widgets_builder/flask_view_controller.py
+++ interactive_widgets_builder/flask_view_controller.py
@@ -1,43 +1,27 @@
-import copy
+from IPython.display import display, clear_output
 import json
+import copy
 
 from jinja2 import Template
 
 from ipywidgets.embed import embed_data
 
+
 from interactive_widgets_builder.view_factory import ViewFactory
 from table_info_extractor.toy_db_info_initialize import ToyDB
 
+from interactive_widgets_builder.ipywidget_view_controller import (
+    SelectionView
+)
+from ipywidgets import VBox
+
 
 class FrontEndParameterHolder():
-  def __init__(self):
-    self.titles = None
-    self.options_collection = None
-    self.ipywidget_info = None
-
-  def set_parameters(self, titles, options_collection, ipywidget_info):
-    self.titles = titles
-    self.options_collection = options_collection
-    self.ipywidget_info = ipywidget_info
-
-  def render_template(self):
-    html_name = "selection_view.html"
-    template = Template(open('templates/' + html_name).read())
-    rendered_template = template.render(
-        titles=self.titles,
-        options_collection=self.options_collection,
-        ipywidget_info=self.ipywidget_info
-    )
-    return rendered_template
-
-
-class ViewController():
-  # template ipython widget embedding
-  def _bridge_ipywidget_to_front_parameters(self, view=None):
-    if view == None:
+  def _convert_ipywidget_to_jinja_parameters(self, ipywidget=None):
+    if ipywidget == None:
       return {'manager_state': None, 'widget_views': [None]}
     else:
-      data = embed_data(views=[view])
+      data = embed_data(views=[ipywidget])
       manager_state = json.dumps(data['manager_state'])
       widget_views = [
           json.dumps(view_spec) for view_spec in data['view_specs']
@@ -47,7 +31,50 @@ class ViewController():
           'widget_views': widget_views
       }
 
-  def __init__(self):
+  def __init__(self, response_func):
+    self.response_func = response_func
+    self.titles = None
+    self.options_collection = None
+    self.ipywidget = None
+
+  def set_parameters(self, titles, options_collection, ipywidget):
+    self.titles = titles
+    self.options_collection = options_collection
+    self.ipywidget = ipywidget
+
+  def render_template(self):
+    html_name = "selection_view.html"
+    template = Template(open('templates/' + html_name).read())
+    rendered_template = template.render(
+        titles=self.titles,
+        options_collection=self.options_collection,
+        ipywidget_info=self._convert_ipywidget_to_jinja_parameters(
+            self.ipywidget
+        )
+    )
+    return rendered_template
+
+  def display_ipywidgets(self):
+    clear_output(wait=True)
+    views = []
+    for title in self.titles:
+      sv = SelectionView(
+          title,
+          self.options_collection[title],
+          self.response_func
+      )
+      views.append(sv.get_widget())
+    if self.ipywidget:
+      views.append(self.ipywidget)
+    display(VBox(views))
+
+
+class ViewController():
+  # template ipython widget embedding
+
+  def __init__(self, platform='flask'):
+    assert platform == 'flask' or platform == 'jupyter'
+    self.platform = platform
     self.dropdown_titles = ['Role', 'PM', 'Member']
     self._options_for_each_dropdown_titles = {
         'Role': ['Admin', 'PM', 'Member'],
@@ -56,10 +83,10 @@ class ViewController():
     }
     self.view_factory = ViewFactory()
     self.entry_dropdown_title = 'Role'
-    self.fep_holder = FrontEndParameterHolder()
+    self.fep_holder = FrontEndParameterHolder(self.response_by_altering_view)
 
   def _get_reordered_option_list(self, dropdown_title, selected_result):
-    import copy
+
     options = copy.copy(
         self._options_for_each_dropdown_titles[dropdown_title])
     options.remove(selected_result)
@@ -71,11 +98,13 @@ class ViewController():
         options_collection={
             'Role': self._options_for_each_dropdown_titles[self.entry_dropdown_title]
         },
-        ipywidget_info=self._bridge_ipywidget_to_front_parameters(None)
+        ipywidget=None
     )
+    if self.platform == 'jupyter':
+      self.fep_holder.display_ipywidgets()
 
   def response_by_altering_view(self, request):
-    import copy
+
     # clear_output(wait=True)
     if 'Role' in request.values.keys():
       if request.values['Role'] == 'PM':
@@ -85,7 +114,7 @@ class ViewController():
                 'Role': self._get_reordered_option_list('Role', 'PM'),
                 'PM': self._options_for_each_dropdown_titles['PM']
             },
-            ipywidget_info=self._bridge_ipywidget_to_front_parameters(None)
+            ipywidget=None
         )
       elif request.values['Role'] == 'Member':
         self.fep_holder.set_parameters(
@@ -94,7 +123,7 @@ class ViewController():
                 'Role': self._get_reordered_option_list('Role', 'Member'),
                 'Member': self._options_for_each_dropdown_titles['Member']
             },
-            ipywidget_info=self._bridge_ipywidget_to_front_parameters(None)
+            ipywidget=None
         )
       else:  # Role: Admin
         self.fep_holder.set_parameters(
@@ -102,9 +131,7 @@ class ViewController():
             options_collection={
                 'Role': self._get_reordered_option_list('Role', 'Admin')
             },
-            ipywidget_info=self._bridge_ipywidget_to_front_parameters(
-                self.view_factory.build_view('all')
-            )
+            ipywidget=self.view_factory.build_view('all')
         )
     else:
       if 'PM' in request.values.keys():
@@ -115,9 +142,7 @@ class ViewController():
                 'Role': self._get_reordered_option_list('Role', 'PM'),
                 'PM': self._get_reordered_option_list('PM', selected_project)
             },
-            ipywidget_info=self._bridge_ipywidget_to_front_parameters(
-                self.view_factory.build_view('project', selected_project)
-            )
+            ipywidget=self.view_factory.build_view('project', selected_project)
         )
         # print("Show PM Table View for", project)
 
@@ -129,12 +154,12 @@ class ViewController():
                 'Role': self._get_reordered_option_list('Role', 'Member'),
                 'Member': self._get_reordered_option_list('Member', selected_member)
             },
-            ipywidget_info=self._bridge_ipywidget_to_front_parameters(
-                self.view_factory.build_view('member', selected_member)
-            )
+            ipywidget=self.view_factory.build_view('member', selected_member)
         )
       else:
         print("Error: no such selection...")
+    if self.platform == 'jupyter':
+      self.fep_holder.display_ipywidgets()
 
 
 '''
